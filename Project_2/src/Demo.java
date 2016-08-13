1d07848755b65e5c99ab3486dad73f6b5f35cca7
Roy <Roy@Roy-PC>
2016-08-12 20:27:06 -0400

    2nd

---
+++ Project_2/src/Demo.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Demo {
+
+public static void main(String[] args)
+	{
+		ParkingLotOwner Roy = new ParkingLotOwner("Roy");
+		ParkingLot ParkingLot = new ParkingLot(10,Roy);
+		Scanner k = new Scanner(System.in);
+		Scanner ky = new Scanner(System.in);
+		int m = 0;
+		while(true)
+		{
+			
+			System.out.println("\nWelcome, what is your business here?"
+					+ "\n1. Park the car."
+					+ "\n2. Unpark the car."
+					+ "\n3. Space available."
+					+ "\n4. Show all cars."
+					+ "\n5. Exit.");
+			
+			m = k.nextInt();
+			
+		if(m < 1 || m > 5)
+					{
+							System.out.println("Error! out of bound, please select again: ");
+							m = k.nextInt();
+					}
+		
+		if(m == 1){
+					System.out.println("Please enter name for your car: ");
+					System.out.println();
+					Car car = new Car(ky.nextLine());
+					
+					ParkingLot.parkCar(car);
+					continue;
+				  }
+		if(m == 2){
+					System.out.println("Please enter the ticket number: ");
+					System.out.println();
+					m = (int) k.nextDouble();
+					Ticket newTicket = new Ticket(m);
+					ParkingLot.unparkCar(newTicket);
+					continue;
+				}
+		if(m == 3){
+					System.out.println("total 10 parking space. currently space taken: " + ParkingLot.getNumberOfCarsParked());
+					continue;
+					}
+		
+		if(m == 4){
+					ParkingLot.showAllCar();
+					continue;
+					}
+		
+		if(m == 5){
+					System.out.println("Good Bye!");
+					break;
+					}
+		
+			
+		}
+	}
+}
---
+++ Project_2/src/ParkingLot.java
@@ -0,0 +1,99 @@
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class ParkingLot {
+
+	private final int capacity;
+	
+	private int numberOfCarsParked;
+		
+	private Map<Ticket,Car> parkedCarMap = new HashMap<Ticket,Car>();	
+	
+	private Map<Car,Ticket> ticketMap = new HashMap<Car,Ticket>();	
+
+	LinkedList<Car> maxSpace = new LinkedList<Car>();
+	 
+	
+	private ParkingLotOwner owner;
+	
+	public ParkingLot(int capacity)
+	{	
+		this.capacity = capacity;
+	}
+	
+	public ParkingLot(int capacity,ParkingLotOwner owner)
+	{	
+		
+		this.capacity = capacity;
+		this.owner=owner;	
+	}
+
+	public int getNumberOfCarsParked()
+	{
+		return numberOfCarsParked;
+	}
+	public boolean hasParkingSpace()
+	{
+		return numberOfCarsParked!=this.capacity;	
+	}	
+
+	public Ticket getTicket(Car car)
+	{
+
+		return ticketMap.get(car);
+	}
+	
+	public void parkCar(Car car)
+	{
+
+		if(hasParkingSpace() && !parkedCarMap.containsValue(car) )
+		{				
+			Ticket newTicket = new Ticket(Math.random() * 100);
+			this.parkedCarMap.put(newTicket,car);
+			this.ticketMap.put(car,newTicket);
+			
+			maxSpace.add(car);
+			numberOfCarsParked++;
+			
+			System.out.println("Your car " + car + " has parked, " + newTicket);
+			
+			
+			if(!hasParkingSpace())
+			{				
+				owner.isNotifiedFull();
+			}			
+			
+		}
+	
+	}
+	
+	public void unparkCar(Ticket t)
+	{
+		if(this.ticketMap.containsValue(t))
+		{	
+			Car unparkedCar = parkedCarMap.get(t);
+			this.parkedCarMap.remove(getTicket(unparkedCar));
+			this.ticketMap.remove(unparkedCar);
+			maxSpace.remove(unparkedCar);
+			numberOfCarsParked--;
+			System.out.println(unparkedCar + " has been unParked, ");
+		}
+	}
+	
+	public void showAllCar()
+	{
+		for(int i = 0; i < numberOfCarsParked; i++)
+		{
+			System.out.println(maxSpace.get(i));
+		}
+	}
+	public String toString()
+	{
+		String x ="";
+		x = "number of car parked: " + getNumberOfCarsParked();
+		return x;
+	}
+	
+}
