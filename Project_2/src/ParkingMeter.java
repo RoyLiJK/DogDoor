71d59412f55ec397bbde8bcae75580d8d252dc75
Roy <Roy@Roy-PC>
2016-08-12 20:20:08 -0400

    1st

---
+++ Project_2/src/ParkingMeter.java
@@ -0,0 +1,23 @@
+
+public class ParkingMeter
+{
+	private int minutesPurchased; 
+	
+	public ParkingMeter(int aMinutesPurchased )
+	{
+		minutesPurchased = aMinutesPurchased;
+	}
+	
+	public int getMinutesPurchased()
+    {
+    	return minutesPurchased;
+    }
+	
+	
+	public void setMinutesPurchased(int aMinutesPurchased)
+    {
+		minutesPurchased = aMinutesPurchased;
+    }
+	
+	
+}
